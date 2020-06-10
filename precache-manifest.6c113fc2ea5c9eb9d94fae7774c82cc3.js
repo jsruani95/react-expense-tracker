37d9a0efdbafa92fb858aeef88c4e1ca8a1adf06
Ruani, Juan Sebastián <45884702+jsruani95@users.noreply.github.com>
2020-06-10 23:13:38 +0200

    Add files via upload

---
+++ precache-manifest.6c113fc2ea5c9eb9d94fae7774c82cc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05fabd836878c87f0c8c10a4fe8f82c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7bd0721021adc6038f7b",
+    "url": "./static/css/main.253372c2.chunk.css"
+  },
+  {
+    "revision": "5c245d49ae3fb81799d1",
+    "url": "./static/js/2.3b71a1a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3b71a1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd0721021adc6038f7b",
+    "url": "./static/js/main.a41fc260.chunk.js"
+  },
+  {
+    "revision": "c58e26e7f7c13e522b5e",
+    "url": "./static/js/runtime-main.3984a92d.js"
+  }
+]);
